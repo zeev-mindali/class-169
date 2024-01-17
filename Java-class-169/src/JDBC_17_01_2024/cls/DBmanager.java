1ef00573efaeb554cbc02f4ee28a086badc15529
Your Name <youremail@yourdomain.com>
2024-01-17 11:23:17 +0200

    connection pool

---
+++ Java-class-169/src/JDBC_17_01_2024/cls/DBmanager.java
@@ -0,0 +1,11 @@
+package JDBC_17_01_2024.cls;
+
+public class DBmanager {
+    //connection credentials to the Data Base
+    public static final String URL = "jdbc:mysql://localhost:3306";
+    public static final String SQL_USER = "root";
+    public static final String SQL_PASSWORD = "12345678";
+
+    //database name (schema)
+    public static final String SQL_DB = "class169";
+}
---
+++ Java-class-169/src/JDBC_17_01_2024/cls/SimpleTester.java
@@ -0,0 +1,7 @@
+package JDBC_17_01_2024.cls;
+
+public class SimpleTester {
+    public static void main(String[] args) {
+
+    }
+}
