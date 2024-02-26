b53d4436d869e4775d603b8e7d75ea01ab665c0d
zeev mindali <mindalizeev@gmail.com>
2024-02-26 10:06:44 +0200

    rest template

--- spring/spring_14_02_24/catbackend/src/main/java/com/johnbryce/catbackend/clr/SmartDialect.java
+++ spring/spring_14_02_24/catbackend/src/main/java/com/johnbryce/catbackend/clr/SmartDialect.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-@Component
+//@Component
 @Order(5)
 public class SmartDialect implements CommandLineRunner {
     @Autowired
