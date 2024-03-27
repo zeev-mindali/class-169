85186052e8904fb1759759fd52ea40a6f63a308c
zeev <mindalizeev@gmail.com>
2024-03-27 13:15:39 +0200

    changes

--- spring/songs/src/main/java/com/mindali/songs/config/OpenAPIConfiguration.java
+++ spring/songs/src/main/java/com/mindali/songs/config/OpenAPIConfiguration.java
@@ -21,7 +21,7 @@ public class OpenAPIConfiguration {
     public OpenAPI defineOpenAPI(@Value("springdoc-openapi-ui") String serviceTitle, @Value("1.6.12") String serviceVersion){
         Server server = new Server();
         server.setUrl("http://localhost:8080");
-        server.setDescription("our youtube api for development, shani the queen, her husband very lucy (we will pray for him)");
+        server.setDescription("our youtube api for development, shani the queen, her husband very lucy !!!");
 
         Contact myContact = new Contact();
         myContact.setName("Zeev Mindali");
